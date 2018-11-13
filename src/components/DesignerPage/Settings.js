287adc5244427715dd7f14e70fdb1121a76f51b9
cdiezmoran <cdiezmoran@gmail.com>
2018-11-12 22:41:36 -0600

    Customize info section

--- src/components/DesignerPage/Settings.js
+++ src/components/DesignerPage/Settings.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import uuid from 'uuid/v4';
 import { bool, func, string } from 'prop-types';
 import shapes from '@shapes';
 import styles from './Settings.scss';
+
+import { estados } from '@fixtures';
 
 import Section from './SettingsSection';
 import ColorPicker from '../ColorPicker';
@@ -18,25 +21,36 @@ const Settings = ({
   position,
   setState,
   display,
-  user: { stylesheet, profilePic },
+  user: { stylesheet, profilePic, brandName, address },
   setStylesheet,
   updateUser
 }) => {
+  const handleChange = ({ target: { name, value } }) => updateUser({ [name]: value });
+
+  const handleAddressChange = ({ target: { name, value } }) => {
+    const newAddress = { ...address, [name]: value };
+    updateUser({ address: newAddress });
+  }
+
   const handleFileChange = ({ target: { name, files } }) => {
     if (files && files[0]) {
-    const reader = new FileReader();
+      const reader = new FileReader();
 
-    reader.onload = ({ target: { result } }) => {
-      // update image
-      const properties = name === 'bgImage'
-        ? { stylesheet: { ...stylesheet, bgImage: result } }
-        : { profilePic: result };
-      updateUser(properties);
+      reader.onload = ({ target: { result } }) => {
+        // update image
+        const properties = name === 'bgImage'
+          ? { stylesheet: { ...stylesheet, bgImage: result } }
+          : { profilePic: result };
+        updateUser(properties);
+      }
+
+      reader.readAsDataURL(files[0]);
     }
+  }
 
-    reader.readAsDataURL(files[0]);
-  }
-  }
+  const renderOptions = () => estados.map(estado => (
+    <option key={uuid()} value={estado}>{estado}</option>
+  ));
 
   return (
     <div className={`${positionStyles[position]} ${display ? styles.display : {}}`}>
@@ -121,7 +135,26 @@ const Settings = ({
         </div>
       </Section>
       <Section title="Informacion" display={display}>
-        info
+        <label htmlFor="brandNameInput" className={styles.inputContainer}>
+          Brand Name
+          <input id="brandNameInput" type="text" name="brandName" value={brandName} onChange={handleChange} />
+        </label>
+        <label htmlFor="cityInput" className={styles.inputContainer}>
+          Ciudad
+          <input id="cityInput" type="text" name="city" value={address.city} onChange={handleAddressChange} />
+        </label>
+        <label className={styles.inputContainer} htmlFor="stateSelect"> {/* eslint-disable-line */}
+          Estado
+          <select
+            id="stateSelect"
+            className={styles.select}
+            value={address.state}
+            onChange={handleAddressChange}
+            name="state"
+          >
+            {renderOptions()}
+          </select>
+        </label>
       </Section>
       <Section title="Contenido" display={display}>
         content
